cc8c620ff2d1e8dfb32d941d5b4b537c76fe5b31
Shelyugov <mr.samuel777@gmail.com>
2017-08-13 16:44:05 +0300

    Improve physics engine

--- scripts/physics-engine.js
+++ scripts/physics-engine.js
@@ -1,26 +1,68 @@
 const engine = {
+	minDelta: 0.1,
 	minSpeed: 0.1,
+	maxStates: 1000,
 
-	recalculateState: function(env, dt) {
-		applyForces(env, dt);
-		moveAllObjects(env, dt);
+	calculateSimulation: function(env, dt) {
+		if (dt <= 0) {
+			return;
+		}
+		while(canContinueCalculation(env)) {
+			var state = $.extend({}, env.states[env.states.length - 1]);
+			recalculateState(env, state, dt);
+			env.states.push(state);
+		}
 	},
 
-	moveAllObjects: function(env, dt) {
-		env.objects.forEach(function(o) {
+	createEnv: function(gCoef, initialState) {
+		return {
+			gCoefficient: gCoef,
+			states: [initialState]
+		};
+	},
+
+	createInitialState: function(x0, y0, vx0, vy0) {
+		return {
+			objects: [{
+				x: x0,
+				y: y0,
+				v: {
+					x: vx0,
+					y: vy0
+				}
+			}]
+		};
+	}
+
+	canContinueCalculation(env) {
+		return env.states.length < maxStates;
+	},
+
+	recalculateState: function(env, state, dt) {
+		applyForces(env, state, dt);
+		moveAllObjects(env, state, dt);
+	},
+
+	moveAllObjects: function(env, state, dt) {
+		state.objects.forEach(function(o) {
 			if (Math.abs(o.v.x) > minSpeed) {
 				o.x += o.v.x;
+			} else {
+				o.v.x = 0;
 			}
 			if (Math.abs(o.v.y) > minSpeed) {
 				o.y = Math.abs(o.y + o.v.y);
+			} else if (Math.abs(o.y) < minDelta) {
+				o.y = 0;
+				o.v.y = 0;
 			}
 		});
 	},
 
-	applyForces: function(env, dt) {
-		env.objects.forEach(function(o) {
-			o.forces.forEach(function(force) {
-				force.applyTo(env, o.v, dt);
+	applyForces: function(env, state, dt) {
+		state.objects.forEach(function(o) {
+			$.each(forces, function(name, force) {
+				force.applyTo(env, o, dt);
 			});
 		});
 	},
@@ -30,7 +72,8 @@ const forces = {
 	// Сила сопротивления воздуха
 	airResistanceForce: {
 		coef: 0.3, // Сила сопротивления воздуха
-		applyTo: function(env, v, dt) {
+		applyTo: function(env, o, dt) {
+			const v = o.v;
 			v.x -= Math.sign(v.x) * Math.pow(v.x, 2) * coef;
 			v.y -= Math.sign(v.y) * Math.pow(v.y, 2) * coef;
 		}
@@ -38,8 +81,17 @@ const forces = {
 
 	// Сила гравитации
 	gravityForce: {
-		applyTo: function(env, v, dt) {
-			v.y -= env.gCoeffient * dt;
+		applyTo: function(env, o, dt) {
+			o.v.y -= o.mass * env.gCoeffient * dt;
+		}
+	},
+
+	// Сила реакции опоры
+	supportReactionForce: {
+		applyTo: function(env, o, dt) {
+			if (o.y == 0) {
+				o.v.y += o.mass * env.gCoefficient * dt;
+			}
 		}
 	}
 };
